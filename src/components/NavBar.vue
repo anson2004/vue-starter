226a0e4ff1fe88f35184dd78f3523010d3e1de7d
Liang <yiqing.liang@storaenso.com>
2019-08-30 23:56:09 +0300

    add nav bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+  <b-nav>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>{{msg}}</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    msg: String
+  }
+}
+</script>
