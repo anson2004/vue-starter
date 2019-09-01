5ef6cfad1fea56c471b421ff702f2527ac011b16
Liang <yiqing.liang@storaenso.com>
2019-09-01 22:55:10 +0300

    modify company page

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -1,7 +1,40 @@
 <template>
- <div>
-   This is the company page
- </div>
+<div class='container-fluid m-4 w-75'>
+  <ul class="list-unstyled mt-4">
+    <b-media tag="li">
+      <b-img slot="aside" :src="require('../assets/lapland1.jpg')"  width="100" alt="placeholder"></b-img>
+
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p class="mb-0">
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+        Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+        ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </p>
+    </b-media>
+
+    <b-media tag="li" class="my-4">
+      <b-img slot="aside"  :src="require('../assets/lapland2.jpg')"  width="100" alt="placeholder"></b-img>
+
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p class="mb-0">
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+        Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+        ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </p>
+    </b-media>
+
+    <b-media tag="li">
+      <b-img slot="aside" :src="require('../assets/lapland3.jpg')" width="100"  alt="placeholder"></b-img>
+
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p class="mb-0">
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+        Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+        ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </p>
+    </b-media>
+  </ul>
+</div>
 </template>
 
 <script>
